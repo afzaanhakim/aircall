b41a4a4bdae8597555e77067043c936f2e1b2550
afzaanhakim <afzaanhakim19@gmail.com>
2022-01-25 18:05:37 -0500

    basic components and css files

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
-
+import ActivityFeed from "./components/ActivityFeed.js";
 import Header from "./Header.jsx";
+import axios from "axios";
 
 const App = () => {
   return (
     <div className="container">
       <Header />
       <div className="container-view">Some activities should be here</div>
+
+      <ActivityFeed />
     </div>
   );
 };
---
+++ src/components/ArchiveList.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ArchiveList = () => {
+  return <div></div>;
+};
+
+export default ArchiveList;
