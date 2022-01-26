22e05291effa6cd462b97809f43902c436c4f164
afzaanhakim <afzaanhakim19@gmail.com>
2022-01-25 21:41:43 -0500

    basic css and layout for all calls list

--- src/components/ActivityFeed.js
+++ src/components/ActivityFeed.js
@@ -1,6 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import ActivityFeedItems from './ActivityFeedItems';
-
+import "../css/ActivityFeed.css"
 
 const ActivityFeed = (props) => {
   const [callState, setCallState] = useState()
@@ -10,7 +10,7 @@ const ActivityFeed = (props) => {
   const callItems = calls.map((call) => {
     return (<ActivityFeedItems key={call.id} call={call} />)
   })
-return <div className="call-list">{callItems}</div>
+return <div className="call-list"><h2>All Calls</h2><div className="call-list-items">{callItems} </div></div>
 };
 
 export default ActivityFeed;
--- src/components/ActivityFeedItems.js
+++ src/components/ActivityFeedItems.js
@@ -1,9 +1,31 @@
-import React from 'react';
+import React from "react";
+import "../css/ActivityFeedItems.css";
+import { FiPhoneIncoming, FiPhoneOutgoing } from "react-icons/fi";
+import ReactTimeago from "react-timeago";
 
 const ActivityFeedItems = (props) => {
-  const {call} = props;
-return <div className="activity-feed-items"><p>{call.direction}</p><p>{call.from}</p><p>{call.call_type}</p></div>
+  const { call } = props;
+
+  return (
+    <div className="activity-feed-items">
+      <div className = "icons-callerInfo">
+      <div className="icons">
+      <p>{call.direction === "inbound" && <FiPhoneIncoming size={20} />}</p>
+      <p>{call.direction === "outbound" && <FiPhoneOutgoing size={20} />}</p>
+      </div>
+      <div className="call-info">
+        <p><strong>{call.from}</strong></p>
+        <p>{call.duration} minutes</p>
+      </div>{" "}
+      </div>
+      <div className="date">
+        {" "}
+        <p>
+          <ReactTimeago date={call.created_at} />{" "}
+        </p>
+      </div>{" "}
+    </div>
+  );
 };
 
 export default ActivityFeedItems;
-
