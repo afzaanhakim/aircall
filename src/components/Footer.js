2f4baa1f5918c2341571e1b7a487aab722ddb1dc
afzaanhakim <afzaanhakim19@gmail.com>
2022-01-27 11:21:05 -0500

    ui fixes to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,17 +2,27 @@ import React from "react";
 import "../css/footer.css";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { RiArchiveDrawerLine } from "react-icons/ri";
-
+import {IoMdKeypad} from "react-icons/io"
 const Footer = (props) => {
-  const {state, setState} = props
+  const { state, setState } = props;
   return (
     <footer>
       <div className="footer-icon">
-        <div className="footer-call-list"> <GiHamburgerMenu size={30} onClick={()=>setState("CURRENT")} /> <p>Call List </p> </div>
-        <div className="footer-archived-list"> 
-        <RiArchiveDrawerLine size={30}  onClick={()=>setState("ARCHIVE")}/>
-        <p>Archived Calls</p>
-         </div>
+        <div className="footer-call-list" onClick={() => setState("CURRENT")}>
+          {" "}
+          <GiHamburgerMenu size={30} /> <p>Call List </p>{" "}
+        </div>
+        <div className="footer-call-list" >
+          {" "}
+          <IoMdKeypad size={30} /> <p>Keypad</p>{" "}
+        </div>
+        <div
+          className="footer-archived-list"
+          onClick={() => setState("ARCHIVE")}
+        >
+          <RiArchiveDrawerLine size={30} />
+          <p>Archived Calls</p>
+        </div>
       </div>
     </footer>
   );
